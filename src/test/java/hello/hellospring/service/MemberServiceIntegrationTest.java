7023d719f9cdca44930147f907717c91bb737b3d
JongHak19 <cj855695@gmail.com>
2023-03-28 16:15:44 +0900

    JdbcTemplateMemberRepository upload

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
-@Transactional
+//@Transactional
 class MemberServiceIntegrationTest {
 
     @Autowired
